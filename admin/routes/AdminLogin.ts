542de946e7940f4464e296a2a55a914b4c231f0e
fernandomema <fernandomema@gmail.com>
2025-07-23 22:23:13 +0200

    Remove SvelteKit project files and replace with EJS views for admin panel

--- admin/routes/AdminLogin.ts
+++ admin/routes/AdminLogin.ts
@@ -1,16 +1,25 @@
-import { Route, RouteMethod } from "zumito-framework";
+import { Route, RouteMethod, ServiceContainer } from "zumito-framework";
 import { AdminLoginCallback } from "./AdminLoginCallback";
+import { AdminAuthService } from "../services/AdminAuthService";
 
 export class AdminLogin extends Route {
 
     method = RouteMethod.get;
     path = '/admin/login';
-    
-    execute(req, res) {
+
+    constructor(
+        private adminAuthService = ServiceContainer.getService(AdminAuthService)
+    ) {
+        super();
+    }
+
+    async execute(req, res) {
+        if (await this.adminAuthService.isLoginValid(req).then(r => r.isValid)) return res.redirect('/admin');    
         const clientId = process.env.DISCORD_CLIENT_ID;
         if (!clientId) throw new Error('DISCORD_CLIET_ID .env var not defined');
-        const domain = req.get('host');
-        const callbackUrl = `https://${domain}/admin/login/callback`;
+        const host = process.env.HOST ??req.get('host');
+        const callbackUrl = `https://${host}/admin/login/callback`;
         res.redirect(`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURI(callbackUrl)}&scope=identify`);
     }
+
 }
