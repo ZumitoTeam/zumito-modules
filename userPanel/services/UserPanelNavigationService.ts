da1591a3c6fa13c928f52d9b7423488d2059bbf3
fernandomema <fernandomema@gmail.com>
2025-07-24 12:41:44 +0200

    feat: Enhance registerSubItems method to include section label for sub-items

--- userPanel/services/UserPanelNavigationService.ts
+++ userPanel/services/UserPanelNavigationService.ts
@@ -28,19 +28,18 @@ export class UserPanelNavigationService {
         return this.items;
     }
 
-    registerSubItems(parentId: string, subItems: NavItem[]) {
-        const parentItem = this.items.find(item => item.id === parentId);
-        if (!parentItem) {
-            throw new Error(`Parent item with id ${parentId} not found`);
+    registerSubItems(parentId: string, sectionLabel: string, subItems: NavItem[]) {
+            const parentItem = this.items.find(item => item.id === parentId);
+            if (!parentItem) {
+                throw new Error(`Parent item with id ${parentId} not found`);
+            }
+            let sectionItem = parentItem.sidebar?.sections.find(section => section.label === sectionLabel);
+            if (!sectionItem) {
+                parentItem.sidebar?.sections.push({ label: sectionLabel, items: [] });
+                sectionItem = parentItem.sidebar?.sections.find(section => section.label === sectionLabel);
+            }
+            sectionItem!.items.push(...subItems);
         }
-        parentItem.sidebar = {
-            showDropdown: true,
-            sections: [{
-                label: parentItem.label,
-                items: subItems,
-            }],
-        };
-    }
 
     /**
      * Returns a copy of the navigation items replacing the `:guildId` token
