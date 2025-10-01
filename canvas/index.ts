abb25fe29163736704bb978630c699a7c6cc2135
fernandomema <fernandomema@gmail.com>
2025-10-02 00:01:34 +0200

    feat(canvas): implement CanvasModule class for module initialization

--- canvas/index.ts
+++ canvas/index.ts
@@ -1,1 +1,10 @@
+import { Module } from 'zumito-framework';
+
 export { CanvasUtils, type CanvasConfig } from './utils/CanvasUtils';
+
+export class CanvasModule extends Module {
+    constructor() {
+        super(import.meta.url);
+        console.log('CanvasModule');
+    }
+}
